921356243072e101ba1347c5345d24513a2efa56
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-09-08 00:27:41 +0300

    create a new sentence

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -1,12 +1,12 @@
-﻿﻿﻿using System;
+﻿using System;
 
 class Program
 {
 	static void Main(string[] args)
 	{
 		string str1 = "Holberton";
-		string str2 = " School";
-	    
-		Console.WriteLine("Welcome to {0}!", (str1 + str2));
+		string str2 = "School";
+		// YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE.
+		Console.WriteLine("Welcome to "+ str1 + " " + str2 + "!");
 	}
 }
