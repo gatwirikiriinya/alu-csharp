1d882fe0b4ac117a180772224f2db822c408d4cb
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-09-07 23:37:58 +0300

    Print float

--- csharp-hello_world/4-print_integer/4-print_integer.cs
+++ csharp-hello_world/4-print_integer/4-print_integer.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿﻿using System;
+
 class Program
 {
 	static void Main(string[] args)
 	{
 		int number = 972;
-        string message = number.ToString() + " Mision Street";
-        Console.WriteLine(message);
+		Console.WriteLine($"{number} Mission Street");
 	}
 }
