f4ab070a8032b17073afdcba263382797629e460
gatwirikiriinya <f.kiriinya@alustudent.com>
2023-09-14 14:25:57 +0300

    Print array of integers

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+public class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if(size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        else if (size == 0)
+        {
+            Console.WriteLine();
+            return new int[0];
+        }
+        else{
+            int[] newArray = new int[size];
+            for ( int i = 0; i < size; i++)
+            {
+                newArray[i] = i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            return newArray;
+        }
+    }
+}
